d1517f37923d712ac5344f0e2af84fbe674f7b31
Jim B <7303480+jimgtbm1020@users.noreply.github.com>
2026-09-02 08:24:38 -0400

    Add global administration loading state (#28)

---
+++ app/loading.tsx
@@ -0,0 +1,2 @@
+export default function Loading(){return <main className="shell"><section className="card"><h1>Backtrace Training Administration</h1><p>Loading administration workspace…</p></section></main>;
+}
